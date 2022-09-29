fbd671408bc853ac9baa159084f2cbfec1a3dd52
Brunopjunq <bruno_junq@poli.ufrj.br>
2022-09-28 23:51:45 -0300

    Add Schemas

---
+++ src/schemas/customersSchema.js
@@ -0,0 +1,10 @@
+import joi from 'joi';
+
+const customersSchema = joi.object({
+    name: joi.string().required(),
+    phone: joi.string().pattern(/[0-9]/).min(10).max(11).required(),
+    cpf: joi.string().required(),
+    birthday: joi.string().pattern(/^\d{4}\-\d{2}\-\d{2}$/).required()
+});
+
+export default customersSchema;
