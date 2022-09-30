b6459370cb8b4fd50fcec63834b44aacd3083171
Brunopjunq <bruno_junq@poli.ufrj.br>
2022-09-29 23:01:02 -0300

    Add getGames

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,6 +1,16 @@
 import connection from "../database/db.js";
 
 export async function getGames(req,res) {
+    try {
+        const games = await connection.query('SELECT games.*, categories.name AS "categoryName" FROM games JOIN categories ON games."categoryId"=categories.id;');
+
+        res.send(games.rows);
+
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+
 
 }
 
--- src/routes/gamesRouter.js
+++ src/routes/gamesRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { postGame } from "../controllers/gamesController.js";
+import { getGames, postGame } from "../controllers/gamesController.js";
 import ValidateGames from "../middleware/ValidateGames.js";
 
 const gamesRouter = Router();
 
 gamesRouter.post('/games', ValidateGames, postGame);
+gamesRouter.get('/games', getGames);
 
 export default gamesRouter;
