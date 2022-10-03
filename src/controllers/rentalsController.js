0c5e8f118814849e5e0d1694dbc871c1abf67bc4
Brunopjunq <bruno_junq@poli.ufrj.br>
2022-10-03 11:51:29 -0300

    Add getRentals

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -18,3 +18,55 @@ export async function postRental(req,res) {
     }
 }
 
+export async function getRentals(req,res) {
+    const { customerId, gameId } = req.query;
+
+    try {
+        const {rows: rentals} = await connection.query(`SELECT 
+        rentals.*,
+        customers.name AS "customerName",
+        customers.id AS "customerId",
+        games.id AS "gameId",
+        games.name AS "gameName", 
+        categories.name AS "categoryName", 
+        categories.id AS "categoryId" 
+        FROM rentals
+        JOIN customers ON rentals."customerId" = customers.id
+        JOIN games ON rentals."gameId" = games.id
+        JOIN categories ON games."categoryId" = categories.id
+        ${customerId ? `WHERE customers.id = ${parseInt(customerId)}` : ""}
+        ${gameId ? `WHERE games.id = ${parseInt(gameId)}` : ""}`);
+
+        const rentalsformat = rentals.map((rental) => {
+            const rentalmod = {
+                ...rental,
+                rentDate: dayjs(rental.rentDate).format("YYYY-MM-DD"),
+                customer: {
+                    id: rental.customerId,
+                    name: rental.customerName,
+                  },
+                  game: {
+                    id: rental.gameId,
+                    name: rental.gameName,
+                    categoryId: rental.categoryId,
+                    categoryName: rental.categoryName,
+                  }
+            }
+
+            delete rentalmod.customerId;
+            delete rentalmod.customerName;
+            delete rentalmod.gameId;
+            delete rentalmod.gameName;
+            delete rentalmod.categoryId;
+            delete rentalmod.categoryName;
+      
+            return rentalmod;
+
+        })
+
+        res.send(rentalsformat);
+        
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { postRental } from "../controllers/rentalsController.js";
+import { getRentals, postRental } from "../controllers/rentalsController.js";
 import ValidateRentals from "../middleware/ValidateRentals.js";
 
 
 const rentalsRouter = Router();
 
 rentalsRouter.post('/rentals', ValidateRentals, postRental);
+rentalsRouter.get('/rentals', getRentals);
 
 export default rentalsRouter;
